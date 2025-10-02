5c5d57407459f16214a5847cee7f11c2ea2c1683
shanu agrawal <agrawalshanu798@gmail.com>
2025-10-02 17:53:59 +0530

    misc fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
 import Login from './features/login/login'
-import Dashboard from './features/dashboard/dashboard'
-import { useAuthUser } from './lib/useAuthUser'
 
 function App() {
-  const { user, loading } = useAuthUser()  
-
-  if (loading) {
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-gray-50">
-        <div className="text-gray-600">Loading...</div>
-      </div>
-    )
-  }
-
-  return user ? <Dashboard /> : <Login />
+  return (
+    <Login />
+  )
 }
 
 export default App
